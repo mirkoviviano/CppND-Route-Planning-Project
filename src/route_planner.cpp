ed323bb318c087925e9fb1a4b8c714b46a7d4af0
mirkoviviano <info@mirkoviviano.it>
2019-05-17 16:58:26 +0100

    Final commit

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -2,22 +2,29 @@
 #include <algorithm>
 
 RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
+	m_Model = model;
 	start_x *= 0.01;
-    start_y *= 0.01;
-    end_x 	*= 0.01;
-    end_y 	*= 0.01;
+  start_y *= 0.01;
+  end_x 	*= 0.01;
+  end_y 	*= 0.01;
   	
 	start_node 	= &model.FindClosestNode(start_x, start_y);
-	end_node	= &model.FindClosestNode(end_x, end_y);
+	end_node		= &model.FindClosestNode(end_x, end_y);
+}
+
+float RoutePlanner::GetDistance(){
+	return distance;
 }
 
 std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node *current_node){
-    std::vector<RouteModel::Node> path_found {};
-    distance = 0.0;
+    std::vector<RouteModel::Node> path_found;
+    distance = 0.0f;
+		RouteModel::Node parent;
 
     while( current_node->parent != nullptr){
         path_found.push_back(*current_node);
-        distance += current_node->distance(*(current_node->parent));
+				parent = *(current_node->parent);
+        distance += current_node->distance(parent); //
         current_node = current_node->parent;
     }
     // While loop ends,so current_node is the starting node of the search as its parent is nullptr.
@@ -25,6 +32,7 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
     distance *= m_Model.MetricScale();
     return path_found;
 }
+
 void RoutePlanner::AStarSearch(){
 	start_node->visited = true;
 	open_list.push_back(start_node);
@@ -32,7 +40,8 @@ void RoutePlanner::AStarSearch(){
 	
   	while(open_list.size() > 0){
 			current_node = NextNode();
-			if(current_node->distance(*end_node) == 0){
+			double distance = current_node->distance(*end_node);
+			if(distance == 0){
 				m_Model.path = ConstructFinalPath(current_node);
 				return;
 			}
@@ -40,9 +49,13 @@ void RoutePlanner::AStarSearch(){
     }
 }
 
+float RoutePlanner::CalculateHValue(RouteModel::Node* node){
+  return node->distance(*end_node);
+}
+
 RouteModel::Node *RoutePlanner::NextNode(){
 	std::sort(open_list.begin(), open_list.end(), [](const auto &_1st, const auto &_2nd){
-      return _1st->h_value * _1st->g_value < _2nd->h_value * _2nd->g_value;
+      return _1st->h_value + _1st->g_value < _2nd->h_value + _2nd->g_value;
     });
   
   	RouteModel::Node *lowest_node = open_list.front();
@@ -51,9 +64,6 @@ RouteModel::Node *RoutePlanner::NextNode(){
   	return lowest_node;
 }
 
-float RoutePlanner::CalculateHValue(RouteModel::Node *node){
-  return node->distance(*end_node);
-}
 
 void RoutePlanner::AddNeighbors(RouteModel::Node *current_node){
 	current_node->FindNeighbors();	
--- src/route_planner.h
+++ src/route_planner.h
@@ -11,20 +11,18 @@ class RoutePlanner {
   public:
     RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
     // Add public variables or methods declarations here.
-	float GetDistance() const {return distance;}
-	void AStarSearch();
+		float GetDistance();
+		void AStarSearch();
   private:
 		std::vector<RouteModel::Node> ConstructFinalPath(RouteModel::Node*);
-    RouteModel &m_Model;
-  	RouteModel::Node *start_node;
-  	RouteModel::Node *end_node;
+    RouteModel& m_Model;
+  	RouteModel::Node* start_node;
+  	RouteModel::Node* end_node;
   	float distance;
-  	
+  	float CalculateHValue(RouteModel::Node*);
 		std::vector<RouteModel::Node*> open_list;
-    RouteModel::Node *NextNode();
-  	
-		void AddNeighbors(RouteModel::Node *current_node);
-  	float CalculateHValue(RouteModel::Node *node);
+    RouteModel::Node* NextNode();
+		void AddNeighbors(RouteModel::Node*);
 };
 
 #endif
